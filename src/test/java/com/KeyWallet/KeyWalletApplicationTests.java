c0bc79ef8c924ef6b8a0cca17491d7e1f0c2b8ff
pawel.wojcik7 <pawel.wojcik7@pollub.edu.pl>
2022-12-04 23:23:47 +0100

    sha512 more core

---
+++ src/test/java/com/KeyWallet/KeyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KeyWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
